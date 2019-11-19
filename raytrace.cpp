616cb40a701d18b111fa593c17c4223d8b94c650
Daniel Nemeth <nemethdanka@gmail.com>
2019-11-19 16:25:17 +0100

    mat4 inverting

--- raytrace.cpp
+++ raytrace.cpp
@@ -1,7 +1,134 @@
 //=============================================================================================
-// Computer Graphics Sample Program: Ray-tracing-let
+// Mintaprogram: Z�ld h�romsz�g. Ervenyes 2019. osztol.
+//
+// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat, BOM kihuzando.
+// Tilos:
+// - mast "beincludolni", illetve mas konyvtarat hasznalni
+// - faljmuveleteket vegezni a printf-et kiveve
+// - Mashonnan atvett programresszleteket forrasmegjeloles nelkul felhasznalni es
+// - felesleges programsorokat a beadott programban hagyni!!!!!!! 
+// - felesleges kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
+// ---------------------------------------------------------------------------------------------
+// A feladatot ANSI C++ nyelvu forditoprogrammal ellenorizzuk, a Visual Studio-hoz kepesti elteresekrol
+// es a leggyakoribb hibakrol (pl. ideiglenes objektumot nem lehet referencia tipusnak ertekul adni)
+// a hazibeado portal ad egy osszefoglalot.
+// ---------------------------------------------------------------------------------------------
+// A feladatmegoldasokban csak olyan OpenGL fuggvenyek hasznalhatok, amelyek az oran a feladatkiadasig elhangzottak 
+// A keretben nem szereplo GLUT fuggvenyek tiltottak.
+//
+// NYILATKOZAT
+// ---------------------------------------------------------------------------------------------
+// Nev    : Nemeth Daniel
+// Neptun : FTYYJR
+// ---------------------------------------------------------------------------------------------
+// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
+// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
+// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
+// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
+// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
+// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
+// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
+// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
+// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
 //=============================================================================================
+
 #include "framework.h"
+//#include <iostream>
+
+//================
+// Okos Float osztály CPP11 labor megoldásaiból: https://cpp11.eet.bme.hu/lab03/#4
+// Nagy részét magam is mefírtam, itt az összehasonlításokhoz fogom használni
+namespace smartfloat{
+	class Float {
+	public:
+		Float() = default;
+		Float(float value) : value_(value) {}
+		explicit operator float() const { return value_; }
+	
+		static constexpr float epsilon = 1e-7f;
+	
+	private:
+		float value_;
+	};
+	
+	
+	Float operator+(Float f1, Float f2) {
+		return float(f1) + float(f2);
+	}
+	
+	Float & operator+=(Float &f1, Float f2) {
+		return f1 = f1 + f2;
+	}
+	
+	Float operator-(Float f1, Float f2) {
+		return float(f1) - float(f2);
+	}
+	
+	Float & operator-=(Float &f1, Float f2) {
+		return f1 = f1 - f2;
+	}
+	Float operator/(Float f1, Float f2) {
+		return float(f1) / float(f2);
+	}
+	
+	Float & operator/=(Float &f1, Float f2) {
+		return f1 = f1 / f2;
+	}
+	Float operator*(Float f1, Float f2) {
+		return float(f1) * float(f2);
+	}
+	
+	Float & operator*=(Float &f1, Float f2) {
+		return f1 = f1 * f2;
+	}
+	
+	/* egyoperandusú */
+	Float operator-(Float f) {
+		return -float(f);
+	}
+	
+	/* kisebb */
+	bool operator<(Float f1, Float f2) {
+		return float(f1) < float(f2)-Float::epsilon;
+	}
+	
+	/* nagyobb: "kisebb" fordítva */
+	bool operator>(Float f1, Float f2) {
+		return f2<f1;
+	}
+	
+	/* nagyobb vagy egyenlő: nem kisebb */
+	bool operator>=(Float f1, Float f2) {
+		return !(f1<f2);
+	}
+	
+	/* kisebb vagy egyenlő: nem nagyobb */
+	bool operator<=(Float f1, Float f2) {
+		return !(f1>f2);
+	}
+	
+	/* nem egyenlő: kisebb vagy nagyobb */
+	bool operator!=(Float f1, Float f2) {
+		return f1 > f2 || f1 < f2;
+	}
+	
+	/* egyenlő: nem nem egyenlő */
+	bool operator==(Float f1, Float f2) {
+		return !(f1 != f2);
+	}
+	
+	
+
+}
+// =====================
+using namespace smartfloat;
+
+bool operator==(const vec2& v1, const vec2& v2){
+	return Float(v1.x)==Float(v2.x) && (Float(v1.y)==Float(v2.y));
+}
+using namespace smartfloat;
+
+// Az alábbiakat a raytracing mintaprogram módosítával készítettem
 
 struct Material {
 	vec3 ka, kd, ks;
@@ -56,6 +183,123 @@ struct Sphere : public Intersectable {
 		hit.material = material;
 		return hit;
 	}
+};
+
+struct mat2{
+	mat2(){};
+	float matrix[2][2];
+	mat2(float m00, float m01, float m10, float m11){
+		matrix[0][0]=m00; matrix[0][1]=m01;
+		matrix[1][0]=m10; matrix[1][1]=m11;
+	}
+	mat2 operator*(const mat2& right) const {
+		mat2 result;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				result.matrix[i][j] = 0;
+				for (int k = 0; k < 2; k++) result.matrix[i][j] += matrix[i][k] * right.matrix[k][j];
+			}
+		}
+		return result;
+	}
+	mat2 operator+(const mat2& right) const {
+		mat2 result;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				result.matrix[i][j] = matrix[i][j]+right.matrix[i][j];
+				
+			}
+		}
+		return result;
+	}
+	mat2 operator*(float a)const{
+		mat2 r;
+		for(int i=0;i<2;++i){
+			for(int j=0;j<2;++j){
+				r.matrix[i][j]=matrix[i][j]*a;
+			}
+		}
+		return r;
+}
+	mat2 operator-(const mat2& right)const{
+		mat2 r=right*float(-1);
+		return *this+r;
+	}
+	mat2 operator-()const{
+		return (*this)*float(-1);
+	}
+
+};
+
+
+mat2 invert(const mat2& mat){
+	float a=mat.matrix[0][0];
+	float b=mat.matrix[0][1];
+	float c=mat.matrix[1][0];
+	float d=mat.matrix[1][1];
+	const mat2 temp{d, -b, -c, a};
+	float mult=1/(a*d-b*c);
+	mat2 t=temp*mult;
+	return t;
+}
+
+mat4 Mat4(mat2 A, mat2 B, mat2 C, mat2 D){
+	mat4 r;
+	r.m[0][0] = A.matrix[0][0]; r.m[0][1] = A.matrix[0][1]; r.m[0][2] = B.matrix[0][0]; r.m[0][3] =B.matrix[0][1];
+	r.m[1][0] = A.matrix[1][0]; r.m[1][1] = A.matrix[1][1]; r.m[1][2] = B.matrix[1][0]; r.m[1][3] = B.matrix[1][1];
+	r.m[2][0] = C.matrix[0][0]; r.m[2][1] = C.matrix[0][1]; r.m[2][2] = D.matrix[0][0]; r.m[2][3] = D.matrix[0][1];
+	r.m[3][0] = C.matrix[1][0]; r.m[3][1] = C.matrix[1][1]; r.m[3][2] =D.matrix[1][0]; r.m[3][3] = D.matrix[1][1];
+	return r;
+}
+
+bool operator==(const mat4& a, const mat4& b){
+	for(int i=0;i<4;++i){
+		for(int j=0;j<4;++j){
+			if(Float(a.m[i][j])!=Float(b.m[i][j])) return false;
+		}
+		
+	}
+	return true;
+}
+
+// https://en.wikipedia.org/wiki/Invertible_matrix#Blockwise_inversion
+mat4 invert(mat4 mat){
+	mat2 A=mat2{mat.m[0][0], mat.m[0][1],
+				mat.m[1][0], mat.m[1][1]};
+	mat2 B=mat2{mat.m[0][2], mat.m[0][3],
+				mat.m[1][2], mat.m[1][3]};
+	mat2 C=mat2{mat.m[2][0], mat.m[2][1],
+				mat.m[3][0], mat.m[3][1]};
+	mat2 D=mat2{mat.m[2][2], mat.m[2][3],
+				mat.m[3][2], mat.m[3][3]};
+	mat2 A_1=invert(A);
+
+	mat2 A_=A_1+A_1*B*invert(D-C*A_1*B)*C*A_1;
+	mat2 B_=-A_1*B*invert(D-C*A_1*B);
+	mat2 C_=-invert(D-C*A_1*B)*C*A_1;
+	mat2 D_=invert(D-C*A_1*B);
+
+	mat4 r=Mat4(A_, B_, C_, D_);
+	return r;
+
+	
+}
+
+
+
+class QuadricSurface: public Intersectable{
+	mat4 matrix;
+	mat4 transformMatrix;
+
+	public:
+		QuadricSurface(mat4 matrix, mat4 transformMatrix, Material* _material):
+			matrix(matrix),
+			transformMatrix(transformMatrix)
+				{material=_material;};
+
+		Hit intersect(const Ray& ray) override{
+			mat4 Q=transformMatrix;
+		};
 };
 
 class Camera {
@@ -213,14 +457,25 @@ public:
 
 FullScreenTexturedQuad * fullScreenTexturedQuad;
 
+void invertTest(){
+	mat4 a{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		if(!(invert(a)*a == TranslateMatrix(vec3(0,0,0)))){
+			printf("%s", "hibás invertálás\n");
+		}
+		else printf("%s", "inverting OK\n");
+}
+
 // Initialization, create an OpenGL context
 void onInitialization() {
+
+	invertTest();
+
 	glViewport(0, 0, windowWidth, windowHeight);
 	scene.build();
 
 	std::vector<vec4> image(windowWidth * windowHeight);
 	long timeStart = glutGet(GLUT_ELAPSED_TIME);
-	scene.render(image);
+	//scene.render(image);
 	long timeEnd = glutGet(GLUT_ELAPSED_TIME);
 	printf("Rendering time: %d milliseconds\n", (timeEnd - timeStart));
 
